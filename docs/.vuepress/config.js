ab9b8f50fe88b13baebef0ac090674949a5853b5
echoyl <inandan@126.com>
2026-08-19 10:08:10 +0800

    feat:将构建后的dist加入版本，以部署pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,20 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defineUserConfig } from 'vuepress'
-import { plumeTheme } from 'vuepress-theme-plume'
-import path from 'node:path'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+import { plumeTheme } from "vuepress-theme-plume";
+import path from "node:path";
 
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: plumeTheme({
-    codeHighlighter: {
-      theme:'github-dark'
-      //themes: { light: 'github-dark', dark: 'github-dark' }, 
-      //themes: { light: 'github-dark', dark: 'vitesse-dark' }, 
-    }
-  }),
-  lang: 'zh-CN',
-  title: 'DeAdmin',
-  description: '基于antdesign laravel 的后台管理系统',
-  source: path.resolve(__dirname, '../'),
-  base:'/doc/'
-})
+	bundler: viteBundler(),
+	theme: plumeTheme({
+		codeHighlighter: {
+			theme: "github-dark",
+			//themes: { light: 'github-dark', dark: 'github-dark' },
+			//themes: { light: 'github-dark', dark: 'vitesse-dark' },
+		},
+	}),
+	lang: "zh-CN",
+	title: "DeAdmin",
+	description: "基于antdesign laravel 的后台管理系统",
+	source: path.resolve(__dirname, "../"),
+	base: "/",
+});
